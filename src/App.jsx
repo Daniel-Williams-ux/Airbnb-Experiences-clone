6f98797f503af39836c6985d88f77b4c02c9e9fe
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-01 06:27:16 +0000

    Hard coded the first section of the card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 function App() {
   return (
     <div>
        <Navbar />
       <Hero />
+      <Card />
     </div>
   );
 }
