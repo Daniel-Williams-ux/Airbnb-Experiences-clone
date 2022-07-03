84939a16a800946131a66f7252d3606ce39999e4
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-03 14:06:53 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,26 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./components/data"
 
 function App() {
+  const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.img}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
   return (
     <div>
        <Navbar />
       <Hero />
-      <Card 
-         img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-        />
+       {cards}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card(props) {
                 <img src="../images/star.png" className="card--star" />
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
