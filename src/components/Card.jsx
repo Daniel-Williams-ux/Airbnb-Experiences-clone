965c577fcc614c09111252abc7112d9e82ae5581
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-10 07:05:40 +0000

    back to previous

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,24 @@
 import React from "react"
 export default function Card(props) {
   let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
   
     return (
         <div className="card">
           {badgeText === 0 && <div className="card--badge">{badgeText}</div>}
-            <img src={`../images/${props.img}`}className="card--image" />
+            <img src={`../images/${props.item.img}`}className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
-                <span>{props.stats.rating}</span>
-                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
                 <span className="gray">{props.item.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
