e96eff691193f66c0b2a01745e0e9dece6bee806
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-10 06:33:03 +0000

    pass objects as prop

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,10 @@ import data from "./components/data"
 
 function App() {
   const cards = data.map(item => {
-    /*Warning: Each child in a list should have a unique "key" prop. Check the render method of `App`. See https://reactjs.org/link/warning-keys for more information. at Card (exe1.bundle.js:194:29) at App  This happens when using data.map elements*/
         return (
             <Card 
-                key={item.id} /*This gets rid of the warning above*/
-                img={item.img}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-                openSpot={item.openSpot}
+                key={item.id}
+                item={item}
             />
         )
     })        
